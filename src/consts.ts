dbe0beba9ed914d1ce7529b00eea75a5a775d63c
Apurva Mishra <apurva.jpr@gmail.com>
2026-02-23 08:28:15 -0800

    Improve website wordings

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Apurva Mishra';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_DESCRIPTION = 'I build ML systems at the compiler–model boundary.';
